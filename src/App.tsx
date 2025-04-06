ee445f4ef508f5e7786f2995743dc318eb994cc4
manpreet07 <manpreet1107@gmail.com>
2025-04-06 09:51:49 -0400

    mf

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import MarsDashboard from "./components/MarsDashboard";
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <MarsDashboard />
+    </div>
+  );
+}
+
+export default App;
