247ee658c754db6b131b29418c94018b26c5d146
manpreet07 <manpreet1107@gmail.com>
2025-04-10 10:06:50 -0400

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,32 +3,28 @@ import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig((config) => {
-  const isProd = config.mode === "production";
-  return {
-    base: isProd ? "/mars-dashboard/" : "/",
-    plugins: [
-      react(),
-      tailwindcss(),
-      federation({
-        name: "marsDashboardApp",
-        filename: "remoteEntry.js",
-        exposes: {
-          "./App": "./src/App.tsx",
-        },
-        shared: ["react", "react-dom", "react-router-dom"],
-        remotes: {},
-      }),
-    ],
-    build: {
-      modulePreload: false,
-      target: "esnext",
-      minify: false,
-      cssCodeSplit: false,
-      outDir: "dist",
-    },
-    server: {
-      port: 5001,
-    },
-  };
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    federation({
+      name: "marsDashboardApp",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App.tsx",
+      },
+      shared: ["react", "react-dom", "react-router-dom"],
+      remotes: {},
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+    outDir: "dist",
+  },
+  server: {
+    port: 5001,
+  },
 });
